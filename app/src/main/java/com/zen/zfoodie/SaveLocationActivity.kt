b45f00c476a40e1994af216644f407101694557d
Zen Wong <zenwong@gmail.com>
2017-07-15 16:14:21 +0800

    added take pics

--- app/src/main/java/com/zen/zfoodie/SaveLocationActivity.kt
+++ app/src/main/java/com/zen/zfoodie/SaveLocationActivity.kt
@@ -2,8 +2,10 @@ package com.zen.zfoodie
 
 import android.Manifest
 import android.annotation.SuppressLint
+import android.app.Activity
 import android.content.Intent
 import android.content.pm.PackageManager
+import android.graphics.BitmapFactory
 import android.location.Location
 import android.net.Uri
 import android.os.Build
@@ -15,17 +17,23 @@ import android.support.v4.app.ActivityCompat
 import android.support.v4.content.FileProvider
 import android.support.v7.app.AppCompatActivity
 import android.util.Log
+import android.view.View
 import com.google.android.gms.location.LocationServices
 import com.google.android.gms.tasks.OnSuccessListener
+import com.squareup.picasso.Picasso
 import kotlinx.android.synthetic.main.save_location.*
 import java.io.File
+import java.io.IOException
 import java.text.SimpleDateFormat
 import java.util.*
+
+
 
 class SaveLocationActivity : AppCompatActivity() {
 	private val REQUEST_PERMISSIONS_REQUEST_CODE = 34
 	private val REQUEST_TAKE_PHOTO = 1
 	private var imgPath : String? = null
+	private var imgUri : Uri? = null
 
 	override fun onCreate(savedInstanceState: Bundle?) {
 		super.onCreate(savedInstanceState)
@@ -46,7 +54,8 @@ class SaveLocationActivity : AppCompatActivity() {
 		}
 
 		tvAddPic.setOnClickListener {
-			dispatchTakePictureIntent()
+			//dispatchTakePictureIntent()
+			testIntent()
 		}
 
 
@@ -104,6 +113,16 @@ class SaveLocationActivity : AppCompatActivity() {
 		return image
 	}
 
+	fun testIntent() {
+		val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+		val imageFile = createImageFile()
+		imgPath = imageFile!!.absolutePath
+		imgUri = Uri.fromFile(imageFile)
+
+		intent.putExtra(MediaStore.EXTRA_OUTPUT, imgUri)
+		startActivityForResult(intent, REQUEST_TAKE_PHOTO)
+	}
+
 	fun dispatchTakePictureIntent() {
 		val takePictureIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
 		// Ensure that there's a camera activity to handle the intent
@@ -113,10 +132,45 @@ class SaveLocationActivity : AppCompatActivity() {
 			// Continue only if the File was successfully created
 			if (photoFile != null) {
 				val photoURI = FileProvider.getUriForFile(this, "com.zen.zfoodie.provider", photoFile)
-				takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
-				startActivityForResult(takePictureIntent, REQUEST_TAKE_PHOTO);
+				val resInfoList = baseContext.getPackageManager().queryIntentActivities(intent, PackageManager.MATCH_DEFAULT_ONLY)
+				for (resolveInfo in resInfoList) {
+					val packageName = resolveInfo.activityInfo.packageName
+					baseContext.grantUriPermission(packageName, photoURI, Intent.FLAG_GRANT_WRITE_URI_PERMISSION or Intent.FLAG_GRANT_READ_URI_PERMISSION)
+				}
+				takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI)
+				startActivityForResult(takePictureIntent, REQUEST_TAKE_PHOTO)
 			}
 		}
+	}
+
+	override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+		Log.d("TEST", "resultCode = $resultCode requestCode = $requestCode")
+		if (requestCode === REQUEST_TAKE_PHOTO && resultCode === Activity.RESULT_OK) {
+			try {
+				//val mImageBitmap = MediaStore.Images.Media.getBitmap(this.contentResolver, Uri.parse(imgPath))
+				//ivMainProfile.setImageBitmap(mImageBitmap)
+
+				Picasso.with(baseContext).load(imgUri).fit().centerCrop().into(ivMainProfile)
+
+				tvAddPic.visibility = View.GONE
+				ivMainProfile.visibility  = View.VISIBLE
+			} catch (e: IOException) {
+				e.printStackTrace()
+			}
+
+		}
+
+//		tvAddPic.visibility = View.GONE
+//		ivMainProfile.visibility  = View.VISIBLE
+//		ivMainProfile.setImageURI(imgUri)
+//
+//		if(data != null) {
+//			val photo = data.extras.get("data") as Bitmap
+//			ivMainProfile.setImageBitmap(photo)
+//		}
+//
+//		Log.d("TEST", imgUri.toString())
+		//Picasso.with(baseContext).load(imgUri).into(ivMainProfile)
 	}
 
 	private fun galleryAddPic() {
@@ -126,5 +180,29 @@ class SaveLocationActivity : AppCompatActivity() {
 		mediaScanIntent.data = contentUri
 		this.sendBroadcast(mediaScanIntent)
 	}
+
+	fun setPic() {
+    // Get the dimensions of the View
+    val targetW = ivMainProfile.getWidth()
+    val targetH = ivMainProfile.getHeight()
+
+    // Get the dimensions of the bitmap
+    val bmOptions = BitmapFactory.Options()
+    bmOptions.inJustDecodeBounds = true
+    BitmapFactory.decodeFile(imgPath, bmOptions)
+    val photoW = bmOptions.outWidth
+    val photoH = bmOptions.outHeight
+
+    // Determine how much to scale down the image
+    val scaleFactor = Math.min(photoW/targetW, photoH/targetH)
+
+    // Decode the image file into a Bitmap sized to fill the View
+    bmOptions.inJustDecodeBounds = false
+    bmOptions.inSampleSize = scaleFactor
+    bmOptions.inPurgeable = true
+
+    val bitmap = BitmapFactory.decodeFile(imgPath, bmOptions)
+    ivMainProfile.setImageBitmap(bitmap)
+}
 
 }
